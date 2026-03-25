a8bb62d2ec7da84e87baa0a5265bd7e60a032413
arnolder <arnoldermorozov@yandex.ru>
2026-03-25 22:40:47 +0300

    feat: создал структуру с лейаутами

---
+++ app/layouts/app-header.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2>Header</h2>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <AppHeader />
+    <slot />
+    <AppFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppFooter from './app-footer.vue';
+import AppHeader from './app-header.vue';
+
+</script>
+
+<style scoped></style>
