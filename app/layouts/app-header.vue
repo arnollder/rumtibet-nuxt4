5eb40a7baaafcf79435f261fc759d4f28534cc75
arnolder <arnoldermorozov@yandex.ru>
2026-03-25 22:57:58 +0300

    feat: подключил global.css, фотовое изображение для хедера

--- app/layouts/app-header.vue
+++ app/layouts/app-header.vue
@@ -1,7 +1,16 @@
 <template>
-  <h2>Header</h2>
+  <div class="root-header">
+    <h2>Header</h2>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.root-header {
+  background-image: url("../assets/images/background-header.webp");
+  background-size: cover;
+  background-position: center;
+  min-height: 600px;
+}
+</style>
