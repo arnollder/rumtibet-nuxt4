0a9383c181e55e941ea3aa90c15c981183052061
arnollder <arnoldermorozov@gmail.ru>
2026-03-26 16:22:43 +0300

    feat: хедер адатив

--- app/layouts/app-header.vue
+++ app/layouts/app-header.vue
@@ -1,36 +1,66 @@
 <template>
   <div class="root-header">
-    <div class="header-line">
-      <img src="../assets/images/logo.svg" alt="logo" />
-      <nav v-if="headerMenu" class="header-menu">
-        <ul class="header-list">
-          <img
-            @click="toggleHeaderMenu"
-            class="close-btn"
-            src="../assets/images/close_icon.svg"
-            alt="close"
-          />
-          <li><a href="#">Про гида</a></li>
-          <li><a href="#">Программа тура</a></li>
-          <li><a href="#">Стоимость</a></li>
-          <li><a href="#">Блог</a></li>
-          <li><a href="#">Контакты</a></li>
-        </ul>
-      </nav>
-      <img
-        @click="toggleHeaderMenu"
-        src="../assets/images/burger.svg"
-        alt="burger-menu"
-      />
+    <div :class="rootContainer">
+      <div :class="headerLine">
+        <img class="header-logo" src="../assets/images/logo.svg" alt="logo" />
+        <nav class="header-menu">
+          <ul v-if="headerMenu || isDesktop" :class="headerList">
+            <img
+              v-if="isMobile || isTablet"
+              @click="toggleHeaderMenu"
+              class="close-btn"
+              src="../assets/images/close_icon.svg"
+              alt="close"
+            />
+            <li><a href="#">Про гида</a></li>
+            <li><a href="#">Программа тура</a></li>
+            <li><a href="#">Стоимость</a></li>
+            <li><a href="#">Блог</a></li>
+            <li><a href="#">Контакты</a></li>
+            <li v-if="isDesktop">
+              <a class="buttons-dark" href="#">Консультация</a>
+            </li>
+          </ul>
+        </nav>
+        <img
+          v-if="isMobile || isTablet"
+          @click="toggleHeaderMenu"
+          src="../assets/images/burger.svg"
+          alt="burger-menu"
+        />
+      </div>
+      <h2 :class="headerTitle">
+        Насладись прогулкой в горы с&nbsp;командой единомышленников
+      </h2>
+      <a v-if="isMobile || isTablet" class="buttons-white" href="#">Найти программу</a>
     </div>
-    <h2 class="header-title">
-      Насладись прогулкой в горы с&nbsp;командой единомышленников
-    </h2>
-    <a class="buttons" href="#">Найти программу</a>
   </div>
 </template>
 
 <script setup lang="ts">
+const { breakpoint } = useViewport();
+
+const isMobile = computed(() => breakpoint.value === "mobile");
+const isTablet = computed(() => breakpoint.value === "tablet");
+
+const isDesktop = computed(() => breakpoint.value === "desktop");
+
+const headerLine = computed(() => ({
+  "header-line-mobile": isMobile.value || isTablet.value,
+  "header-line-desktop": isDesktop.value,
+}));
+const headerList = computed(() => ({
+  "header-list-mobile": isMobile.value || isTablet.value,
+  "header-list-desktop": isDesktop.value,
+}));
+const rootContainer = computed(() => ({
+  "root-container": isDesktop.value,
+}));
+const headerTitle = computed(() => ({
+  "header-title-mobile": isMobile.value || isTablet.value,
+  "header-title-desktop": isDesktop.value,
+}));
+
 const headerMenu = ref(false);
 const toggleHeaderMenu = () => {
   headerMenu.value = !headerMenu.value;
@@ -46,12 +76,20 @@ const toggleHeaderMenu = () => {
   background-position: center;
   min-height: 600px;
 }
-.header-line {
+.header-logo {
+  cursor: pointer;
+}
+.header-line-mobile {
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
-.header-title {
+.header-line-desktop {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.header-title-mobile {
   margin-top: 298px;
   margin-bottom: 32px;
   color: white;
@@ -61,13 +99,17 @@ const toggleHeaderMenu = () => {
   font-weight: 700;
   line-height: 1.5;
 }
-.buttons {
-  font-family: "NunitoSans";
+.header-title-desktop {
+  margin-top: 298px;
+  margin-bottom: 32px;
+  color: white;
+  text-transform: uppercase;
+  font-family: "NEXT_ART_Bold";
+  font-size: 50px;
   font-weight: 700;
-  color: rgba(26, 62, 62, 1);
-  background-color: white;
+  line-height: 1.5;
 }
-.header-list {
+.header-list-mobile {
   display: flex;
   flex-direction: column;
   row-gap: 16px;
@@ -82,10 +124,21 @@ const toggleHeaderMenu = () => {
   font-family: "NunitoSans";
   font-weight: 400;
 }
+.header-list-desktop {
+  display: flex;
+  column-gap: 24px;
+  color: white;
+  font-family: "NunitoSans";
+  font-weight: 600;
+  font-size: 18px;
+}
 .close-btn {
   width: 24px;
   height: 24px;
   display: flex;
   margin-left: auto;
+}
+.buttons-dark {
+  margin-left: 25px;
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,23 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/global.css"],
+  modules: [
+    'nuxt-viewport'
+  ],  
+  viewport: {
+    breakpoints: {
+      // Ключи - названия брейкпоинтов, значения - медиазапросы
+      mobile: 390,
+      tablet: 768,
+      desktop: 1024,
+      // wide: 1280,
+    },    
+    // Настройка по умолчанию для SSR
+    defaultBreakpoints: {
+      desktop: 'desktop',
+      mobile: 'mobile',
+    },    
+    // Стратегия: 'mobile-first' или 'desktop-first'
+    fallbackBreakpoint: 'mobile',
+  },
 });
