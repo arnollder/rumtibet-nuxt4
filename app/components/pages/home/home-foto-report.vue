1280a6052bfc5de6527dbbe58b9f6e94618715ae
arnolder <arnoldermorozov@yandex.ru>
2026-04-06 01:01:56 +0300

    feat: home-foto-report

---
+++ app/components/pages/home/home-foto-report.vue
@@ -0,0 +1,48 @@
+<template>
+  <div :class="rootContainer">
+    <div class="root-component">
+      <div :class="title">
+        <h2 class="description-title description-title-mobile">
+          фото-отчет
+        </h2>
+        <h2 class="component-title component-title-mobile">
+          Делимся впечатлениями
+        </h2>
+      </div>
+
+        <a class="buttons-dark" :class="buttons" href="#">Наш pinterest</a>
+
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { breakpoint } = useViewport();
+
+const isMobile = computed(() => breakpoint.value === "mobile");
+const isTablet = computed(() => breakpoint.value === "tablet");
+const isDesktop = computed(() => breakpoint.value === "desktop");
+
+const rootContainer = computed(() => ({
+  "root-container": isDesktop.value,
+}));
+const title = computed(() => ({
+  "title-desktop": isDesktop.value,
+}));
+const buttons = computed(() => ({
+  "buttons-dark-mobile": isMobile.value || isTablet.value,
+  "buttons-dark-desktop": isDesktop.value,
+}));
+</script>
+
+<style>
+.root-component {
+  padding: 50px 16px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 32px;
+}
+.buttons-dark {
+  margin: 0 auto;
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,11 +3,13 @@
   <HomeOffer />
   <HomeScenario />
   <HomeBlog />
+  <HomeFotoReport />
 </template>
 
 <script setup lang="ts">
 import HomeAbout from '~/components/pages/home/home-about.vue';
 import HomeBlog from '~/components/pages/home/home-blog.vue';
+import HomeFotoReport from '~/components/pages/home/home-foto-report.vue';
 import HomeOffer from '~/components/pages/home/home-offer.vue';
 import HomeScenario from '~/components/pages/home/home-scenario.vue';
 
